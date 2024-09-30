3f91c06c2212ab4d566b79d14ef36b7e12bf507b
Nadeem Akhtar <nadakhtar@Nadeems-MacBook-Pro.local>
2024-09-30 14:55:56 +0100

    routing, views, controler for categories

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,5 +3,5 @@ class Bookmark < ApplicationRecord
   belongs_to :category
 
   validates :recipe_id, uniqueness: { scope: :category_id }
-  validates :comment, length { minimum: 6, too_short: "must have more than 6 characters" }
+  validates :comment, length: { minimum: 6, too_short: "must have more than 6 characters" }
 end
